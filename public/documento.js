1e123f0ac953d7c3cf14da3c1caaa071a57cd04e
kaique <kaiquesilva49@hotmail.com>
2024-05-12 17:28:07 -0300

    identificando o documento

--- public/documento.js
+++ public/documento.js
@@ -1,1 +1,26 @@
 const socket = io();
+
+
+
+const textEditor = document.getElementById("editor-texto");
+const params = new URLSearchParams(window.location.search);
+const docName = params.get('nome')
+const docTitle= document.getElementById('titulo-documento')
+
+function selectDocument(nome){
+    socket.emit('selecionar_documento',nome)
+}
+
+
+docTitle.textContent=docName
+
+textEditor.addEventListener("keyup",(e)=>{
+    socket.emit("evento", textEditor.value)
+})
+
+socket.on("evento_documento",(texto)=>{
+    console.log(texto)
+    textEditor.value=texto
+})
+
+selectDocument(docName);
--- src/socket.js
+++ src/socket.js
@@ -2,4 +2,15 @@ import io from "./server.js"
 
 io.on('connection', (socket) => {    
     console.log('socket conectado ID: ' + socket.id)
+    socket.on("selecionar_documento",(nome)=>{
+        console.log(nome)
+    })
+
+
+    socket.on('evento', (data) => {
+        socket.broadcast.emit("evento_documento", data)
+        
+        console.log(data)
+    })
+
 })
